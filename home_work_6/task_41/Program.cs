af5fe0717f449067ad924be4392cdcaca3f89c10
Alexey <info@geekbrains.ru>
2023-01-25 15:31:43 +0300

    ДЗ №6

---
+++ home_work_6/task_41/Program.cs
@@ -0,0 +1,20 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. 
+// Ввод чисел останавливается при помощи ввода стоп-слова "stop" и производится при помощи нажатия Enter
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+string InputNumber = string.Empty;
+int number;
+int count = 0;
+
+while (true){
+    Console.Write("Введите число или stop: ");
+    InputNumber = Console.ReadLine();
+    if (InputNumber == "stop"){
+        break;
+    }
+    number = Convert.ToInt32(InputNumber);
+    if (number > 0)
+        count++;
+}
+
+Console.Write(count);
