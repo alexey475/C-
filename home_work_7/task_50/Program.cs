23176b8e1dda55462b3f6acc4e4c9495824f755e
Alexey <info@geekbrains.ru>
2023-01-26 16:27:43 +0300

    ДЗ №7

---
+++ home_work_7/task_50/Program.cs
@@ -0,0 +1,53 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+int[,] GetRandomMatrix(int rows, int columns, int leftRange, int rightRange){
+    int[,] matrix = new int[rows, columns];
+    for(int i = 0; i < matrix.GetLength(0); i++){
+        for(int j = 0; j < matrix.GetLength(1); j++){
+            matrix[i, j] = new Random().Next(leftRange, rightRange + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix){
+    for(int i = 0; i < matrix.GetLength(0); i++){
+        for(int j = 0; j < matrix.GetLength(1); j++){
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Coordinates(int[,] matrix){
+    string? Answer = string.Empty;
+    int Rows;
+    int Columns;
+    while (true){
+        Console.WriteLine("Проверить позиции элемента в массиве (да/нет)?");
+        Answer =  Console.ReadLine();
+        if (Answer != "да" && Answer != "нет"){
+            continue;
+        }
+        if (Answer == "нет"){
+            break;
+        }
+        Console.Write("Введите номер строки: ");
+        Rows = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите номер столбца: ");
+        Columns = Convert.ToInt32(Console.ReadLine());
+
+        if (Rows > matrix.GetLength(0) || Rows < 1 || Columns > matrix.GetLength(1) || Columns < 1){
+            Console.WriteLine("Такой позиции в массиве нет");
+            Console.WriteLine();
+        }
+        else {
+            Console.WriteLine(matrix[Rows -1, Columns - 1]);
+            Console.WriteLine();
+        }
+    }
+}
+
+int[,] array = GetRandomMatrix(3, 4, 0, 100);
+PrintMatrix(array);
+Coordinates(array);
---
+++ home_work_7/task_52/Program.cs
@@ -0,0 +1,40 @@
+﻿//  Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+int[,] GetRandomMatrix(int rows, int columns, int leftRange, int rightRange){
+    int[,] matrix = new int[rows, columns];
+    for(int i = 0; i < matrix.GetLength(0); i++){
+        for(int j = 0; j < matrix.GetLength(1); j++){
+            matrix[i, j] = new Random().Next(leftRange, rightRange + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix){
+    for(int i = 0; i < matrix.GetLength(0); i++){
+        for(int j = 0; j < matrix.GetLength(1); j++){
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Average(int[,] matrix){
+    double[] PrintAvrg = new double[matrix.GetLength(1)];
+    for(int i = 0; i < matrix.GetLength(1); i++){
+        int sum = 0;
+        double avrg = 0;
+        int count = 0;
+        for(int j = 0; j < matrix.GetLength(0); j++){
+            sum += matrix[j, i];
+            count ++;
+        }
+        avrg = Math.Round((double) sum / count, 1);
+        PrintAvrg[i] = avrg;
+    }
+    Console.WriteLine();
+    Console.WriteLine(string.Join("; ", PrintAvrg));
+}
+
+int[,] array = GetRandomMatrix(3, 4, 0, 10);
+PrintMatrix(array);
+Average(array);
