b5ad11f73572001d4767c76f28e70ea2590f8be0
Alexey <info@geekbrains.ru>
2023-01-31 20:35:22 +0300

    ДЗ №9

---
+++ home_work_9/task_64/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задайте значение N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от N до 1
+int n = 5;
+int m = 1;
+
+void Number(int n, int m){
+    if (n + 1 == m) return;
+    Number(n, m + 1);
+    Console.Write(m + " ");
+}
+
+Number(n, m);
---
+++ home_work_9/task_66/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N
+int m = 1;
+int n = 15;
+
+int SumNumber(int m, int n){
+    if (m == n) return n;
+    return n + SumNumber(m, n - 1);
+}
+
+Console.WriteLine($"Сумма элементов от {m} до {n} = {SumNumber(m, n)}");
